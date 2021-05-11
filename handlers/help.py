04c8d54d3382e7d6ea2dc240f0385096716dda14
alinebot <72397223+alinebot@users.noreply.github.com>
2021-05-11 10:10:20 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -31,10 +31,10 @@ async def help(client: Client, message: Message):
     await message.reply_text(
         f"""❤ The commands and there use is explained here ❤
 - `/saavn` To search song on jio saavan and play the first result 
-- `/ytt` To search the song on Youtube and play the first matching result.
+- `/play` To search the song on Youtube and play the first matching result.
 - `/deezer` To search song on deezer and play good quality stream.
 - `/song` To download a song from youtube.
-- `/play` Reply this in response to a link or any telegram audio file it will be played 
+- `/music` Reply this in response to a link or any telegram audio file it will be played 
 - `/skip` to skip current song 
 - `/stop` to stop the streaming of song 
 - `/pause` to pause the stream 
