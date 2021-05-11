91a0cbe5fbc3d7c6a801b0619b3e0ee656ac33b0
alinebot <72397223+alinebot@users.noreply.github.com>
2021-05-11 09:52:16 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('✯𝗔𝘂𝗿𝗮𝗫✯=🔎 Searching the song...')
+    m = message.reply('✯𝗗𝗘𝗡𝗩𝗜𝗟✯=🔎 Searching the song...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -41,17 +41,17 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "✯𝗔𝘂𝗿𝗮𝗫✯=❌ Found Nothing.\n\nTry another keywork or maybe spell it properly."
+            "✯𝗗𝗘𝗡𝗩𝗜𝗟✯=❌ Found Nothing.\n\nTry another keywork or maybe spell it properly."
         )
         print(str(e))
         return
-    m.edit("✯𝗔𝘂𝗿𝗮𝗫✯=⏬ Downloading the song by @AuraXNetwork...")
+    m.edit("✯𝗗𝗘𝗡𝗩𝗜𝗟✯=⏬ Downloading the song by @MusicBotEnjoy_group...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**✯𝗔𝘂𝗿𝗮𝗫✯=🆙 Uploaded by @AuraXNetwork**'
+        rep = '**✯𝗗𝗘𝗡𝗩𝗜𝗟✯=🆙 Uploaded by @MusicBotEnjoy_group**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -59,7 +59,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('✯𝗔𝘂𝗿𝗮𝗫✯=❌ Error')
+        m.edit('✯𝗗𝗘𝗡𝗩𝗜𝗟✯=❌ Error')
         print(e)
 
     try:
